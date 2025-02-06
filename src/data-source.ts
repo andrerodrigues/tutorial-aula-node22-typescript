762cc316f82646eacc06cfeb12fdada1d0b79d3c
André de Araújo Rodrigues <andre-rodrigues@prodesan.com.br>
2025-02-06 11:32:12 -0300

    Refatoração de código.

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
     name: "sqliteConnection", 
     type: "sqlite",
     database: "database.sqlite",
-    entities: ["src/entity/*.ts"], // Ajuste conforme sua estrutura
+    entities: [User], // Ajuste conforme sua estrutura
+    migrations:[__dirname + "/migration/*.ts"],
     synchronize: true, // True apenas para desenvolvimento
 });
